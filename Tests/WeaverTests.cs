7a2d76d030e1f1a933e97bfb1179ca3618f34509
Nick Chamberlain <nick.chamberlain.jr@gmail.com>
2016-09-19 18:05:25 -0400

    reference assembly and tests

--- Tests/WeaverTests.cs
+++ Tests/WeaverTests.cs
@@ -36,12 +36,35 @@ public class WeaverTests
     }
 
     [Test]
-    public void ValidateHelloWorldIsInjected()
-    {
-        var type = assembly.GetType("Hello");
-        var instance = (dynamic)Activator.CreateInstance(type);
+    public void Pre_weaved_exception_gets_swallowed() {
+        var type = assembly.GetType("AssemblyToProcess.OnException");
+        var instance = (dynamic) Activator.CreateInstance(type);
 
-        Assert.AreEqual("Hello World", instance.World());
+        Assert.DoesNotThrow(() => instance.Swallowed_exception());
+    }
+
+    [Test]
+    public void Pre_weaved_unmatched_catch_statement_swallowed() {
+        var type = assembly.GetType("AssemblyToProcess.OnException");
+        var instance = (dynamic) Activator.CreateInstance(type);
+
+        Assert.DoesNotThrow(() => instance.Swallowed_wrong_type_exception());
+    }
+
+    [Test]
+    public void Expected_weaved_method_throws() {
+        var type = assembly.GetType("AssemblyToProcess.OnException");
+        var instance = (dynamic) Activator.CreateInstance(type);
+
+        Assert.Throws<Exception>(() => instance.Swallowed_exception_to_be_unswallowed());
+    }
+
+    [Test]
+    public void Uncaught_exception_type_gets_rethrown() {
+        var type = assembly.GetType("AssemblyToProcess.OnException");
+        var instance = (dynamic) Activator.CreateInstance(type);
+
+        Assert.Throws<ArgumentException>(() => instance.Swallowed_exception_not_filtered_gets_unswallowed());
     }
 
 #if(DEBUG)
---
+++ UnSwallowExceptionsReferenceAssembly/UnSwallowExceptionsAttribute.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace UnSwallowExceptions.Fody
+{
+    [AttributeUsage(AttributeTargets.Method | AttributeTargets.Constructor, AllowMultiple = false, Inherited = false)]
+    public class UnSwallowExceptionsAttribute : Attribute {
+    }
+}
